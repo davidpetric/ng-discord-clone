a58c6ac3ddc619dc8465a476a91fa03349bbc4bc
David <davidpetric@users.noreply.github.com>
2022-03-03 22:37:59 +0200

    Partial fix  css issues and replace icons with iconmonstr.

--- src/app/guild-navbar/guild-navbar.component.ts
+++ src/app/guild-navbar/guild-navbar.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../services/navbar/navbar.service';
 
 @Component({
   selector: 'app-guild-navbar',
   templateUrl: './guild-navbar.component.html',
-  styleUrls: ['./guild-navbar.component.css']
+  styleUrls: ['./guild-navbar.component.css'],
 })
 export class GuildNavbarComponent implements OnInit {
+  constructor(navbarService: NavbarService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
